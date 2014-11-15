1e2c390f330cef47f6559bb837422dfebd8a822c
David Speck <david@saduras.de>
2014-11-15 15:24:04 +0100

    Improved naming

--- src/windowing/layout/ContainerWithLayout.hx
+++ src/windowing/layout/ContainerWithLayout.hx
@@ -10,22 +10,34 @@ import openfl.display.Sprite;
  */
 class ContainerWithLayout extends Sprite
 {
+	// Selected layout
 	var layout : ILayout;
+	
+	// Container size
+	public var containerWidth : Float;
+	public var containerHeight : Float;
 
-	public function new(layout : ILayout) 
+	public function new(layout : ILayout, width : Float, heigth : Float) 
 	{
 		super();
 		
+		// Initial set layout
 		this.layout = layout;
+		
+		// Assign width & height
+		this.containerWidth = width;
+		this.containerHeight = height;
 	}
 	
+	// Change layout of this container and update the arrangemengt
 	public function setLayout(newLayout : ILayout) : Void
 	{
 		layout = newLayout;
-		updateLayout();
+		applyLayout();
 	}
 	
-	public function updateLayout() : Void
+	// Apply layout to children in this container
+	public function applyLayout() : Void
 	{
 		layout.apply(this);
 	}
@@ -35,41 +47,41 @@ class ContainerWithLayout extends Sprite
 	override public function addChild(child : DisplayObject) : DisplayObject 
 	{
 		var result = super.addChild(child);
-		updateLayout();
+		applyLayout();
 		return result;
 	}
 	
 	override public function addChildAt(child : DisplayObject, index : Int) : DisplayObject 
 	{
 		var result = super.addChildAt(child, index);
-		updateLayout();
+		applyLayout();
 		return result;
 	}
 	
 	override public function removeChild(child : DisplayObject) : DisplayObject 
 	{
 		var result = super.removeChild(child);
-		updateLayout();
+		applyLayout();
 		return result;
 	}
 	
 	override public function removeChildAt(index : Int) : DisplayObject 
 	{
 		var result = super.removeChildAt(index);
-		updateLayout();
+		applyLayout();
 		return result;
 	}
 	
 	override public function removeChildren(beginIndex : Int = 0, endIndex : Int = 0x7FFFFFFF) : Void 
 	{
 		super.removeChildren(beginIndex, endIndex);
-		updateLayout();
+		applyLayout();
 	}
 	
 	override public function setChildIndex(child : DisplayObject, index : Int)  : Void
 	{
 		super.setChildIndex(child, index);
-		updateLayout();
+		applyLayout();
 	}
 	//}
 }
