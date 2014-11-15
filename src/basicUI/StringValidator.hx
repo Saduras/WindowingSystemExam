cdf2170bf0a87d1cdc87101d1b32d06eb1fc09b7
David Speck <david@saduras.de>
2014-11-15 14:37:06 +0100

    Improve regular expression for address

--- src/basicUI/StringValidator.hx
+++ src/basicUI/StringValidator.hx
@@ -21,53 +21,41 @@ class StringValidator
 		regFloat  = ~/^[0-9]+\.[0-9]+$/;
 		regColor = ~/^#[0-9A-Fa-f]{3}$/;
 		regColor2 = ~/^#[0-9A-Fa-f]{6}$/;
-		regAddress = ~/^[A-Z]{1}+[a-zA-Z ]+[ ]{1}+[0-9]+[a-z]{0,1}+$/;
+		regAddress = ~/^([A-Z]{1}+[a-z]+[ ]){1,}+[0-9]+[a-z]{0,1}+$/;
 		this.modus = modus;
 	}
 	
 	public function checkText (text : String ) : Bool
 	{
-		if (modus == "text")
-		{
-			if (regText.match(text))
-			{
-				return true;
-			}
-			
+		switch(modus) {
+			case "text":
+				if (regText.match(text))
+				{
+					return true;
+				}
+			case "number":
+				if (regNumber.match(text))
+				{
+					return true;
+				}
+			case "float":
+				if (regFloat.match(text))
+				{
+					return true;
+				}
+			case "color":
+				if (regColor.match(text) || regColor2.match(text))
+				{
+					return true;
+				}
+			case "address":
+				if (regAddress.match(text))
+				{
+					return true;
+				}
 		}
-		else if (modus == "number")
-		{
-			if (regNumber.match(text))
-			{
-				return true;
-			}
-			
-		}
-		else if (modus == "float")
-		{
-			if (regFloat.match(text))
-			{
-				return true;
-			}
-			
-		}
-		else if (modus == "color")
-		{
-			if (regColor.match(text) || regColor2.match(text))
-			{
-				return true;
-			}
-			
-		}
-		else if (modus == "address")
-		{
-			if (regAddress.match(text))
-			{
-				return true;
-			}
-		}
+		
 		return false;
-			 
 	}
 	
 }
