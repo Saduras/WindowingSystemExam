836d21eb7268574669205895c87c1ed5e2862d20
David Speck <david@saduras.de>
2014-11-15 16:33:23 +0100

    Add multiple column support for vertical layout

--- src/windowing/layout/VerticalLayout.hx
+++ src/windowing/layout/VerticalLayout.hx
@@ -2,7 +2,8 @@ package windowing.layout;
 import openfl.display.Sprite;
 
 /**
- * ...
+ * This layout places as much elements a possible in vertical columns
+ * and centers these columns.
  * @author David Speck
  */
 class VerticalLayout implements ILayout
@@ -11,26 +12,95 @@ class VerticalLayout implements ILayout
 	
 	public function getId():String { return "VERTICAL"; }
 	
-	public function apply(container : Sprite) : Void 
+	// Apply layout by arraning containers children accordently.
+	public function apply(container : ContainerWithLayout) : Void 
 	{
-		var offsetY : Float = calculateStartOffset(container);
+		// Count of child fitting in each column
+		var columnChildCount : Array<Int> = calculateColumnDistribution(container);
+		
+		// Max height for childs in one column
+		var maxChildWidth : Float = -100;
+		
+		// Index of current row
+		var columnIndex : Int = 0;
+		// Index of first child in this column
+		var columnStartIndex : Int = 0;
+		
+		// Offsets applied to position of the childs
+		// Will be updated for each child
+		var offsetX : Float = container.containerPaddingX;
+		var offsetY : Float = calculateStartOffsetY(container, 0, columnChildCount[columnIndex]);
+		
 		for (i in 0...container.numChildren) {
 			var child = container.getChildAt(i);
-			child.x = 0;
+			
+			// Set coordinates
+			child.x = offsetX;
 			child.y = offsetY;
-			offsetY += child.height;
+			
+			// Calculate offset for next child
+			if (i + 1 < container.numChildren) {
+				var nextChild = container.getChildAt(i + 1);
+				if (i + 1 < columnChildCount[columnIndex] + columnStartIndex) {
+					// Set offsets
+					offsetY += nextChild.height;
+					
+					// Update max width
+					if (child.width > maxChildWidth) {
+						maxChildWidth = nextChild.width;
+					}
+				} else {
+					// Update indicies
+					columnIndex++;
+					columnStartIndex = i + 1;
+					
+					// Set offsets
+					offsetX += maxChildWidth;
+					offsetY = calculateStartOffsetY(container, columnStartIndex, columnChildCount[columnIndex]);
+					
+					// Reset max
+					maxChildWidth = -100;
+				}
+			}
 		}
 	}
 	
-	// Calculate offset such that the content will be centered.
-	// If the content is bigger then the container the offest will be 0.
-	private function calculateStartOffset(container : Sprite) : Float
+	// Calculate count of childs fitting in each column.
+	// Put atleast one child in one column.
+	private function calculateColumnDistribution(container : ContainerWithLayout) : Array<Int>
+	{
+		var columns : Array<Int> = new Array<Int>();
+		var j : Int = 0;
+		columns[j] = 0;
+		
+		var sumY : Float = 0;
+		for (i in 0...container.numChildren) {
+			var child = container.getChildAt(i);
+			// Count child to current row if row is empty or container has enough space
+			if (sumY == 0 || sumY + child.height < container.containerHeight - 2 * container.containerPaddingY) {
+				columns[j]++;
+				// Update sumX
+				sumY += child.height;
+			} else {
+				// Count child to new row
+				j++;
+				columns[j] = 1 ;
+				// Reset sum counter
+				sumY = child.height;
+			}
+		}
+		
+		return columns;
+	}
+	
+	// Calculate offset such that the content column will be centered
+	private function calculateStartOffsetY(container : ContainerWithLayout, startIndex : Int, childCount : Int) : Float
 	{
 		var contentHeight : Float = 0;
-		for (i in 0...container.numChildren) {
+		for (i in startIndex...(startIndex + childCount)) {
 			contentHeight += container.getChildAt(i).height;
 		}
 		
-		return (contentHeight > container.height) ? 0 : container.height / 2.0 - contentHeight / 2.0;
+		return (container.containerHeight - container.containerPaddingY) / 2.0 - contentHeight / 2.0;
 	}
 }
